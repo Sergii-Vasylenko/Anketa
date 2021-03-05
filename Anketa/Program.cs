491b69b57e93107d828898001f7a6fe4028fe98f
Sergii <job.sergii.vasylenko@gmail.com>
2021-03-05 13:03:17 +0200

    Add project files.

---
+++ Anketa/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Anketa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+            
+            Console.Write("Enter your age: ");
+            var age = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"Your name is {name} and age is {age}");
+
+            Console.Write("Enter your birthdate: ");
+            var birthdate = Console.ReadLine();
+
+            Console.WriteLine($"Your birthdate is {birthdate}");
+
+            Console.ReadKey();
+        }
+    }
+}
